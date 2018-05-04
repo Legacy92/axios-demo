a4b0075e60a038637d2a95ebbd315471309205ee
Legacy92 <colemancorey22@gmail.com>
2018-05-04 14:16:16 -0700

    Axios demo completed

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 const BASE_URL = 'http://api.reactprototypes.com';
-const API_KEY = '?key=testuser1234';
+const API_KEY = '?key=c118demouser';
 
-axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
-    const { todos } = resp.data;
-    const table = $('table tbody');
+axios.get(`${BASE_URL}/todos${API_KEY}`).then(response => {
+    console.log(response);
 
-    addToDom(todos, table);
+    const listElements = response.data.todos.map((item,index) => {
+        const tr = document.createElement('tr');
+        const number = document.createElement('td');
+        const title = document.createElement('td');
+        const completed = document.createElement('td');
+
+        number.innerText = index + 1;
+        title.innerText = item.title;
+        completed.innertext = item.complete;
+
+        tr.appendChild(number);
+        tr.appendChild(title);
+        tr.appendChild(completed);
+
+        document.querySelector('.table tbody').appendChild(tr);
+    });
 });
 
-const addToDom = (list, container) => {
-    const tableRows = list.map((item, index) => {
+const todoItem = {
+    tittle: 'What Corey needs to do',
+    details: 'Go see Avengers Infinity War'
+}
 
-        const tableData = [
-            $(`<td>${index + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
+axios.post(`${BASE_URL}/todos${API_KEY}`, todoItem).then(resp => {
+    console.log('Add Item Resp:', resp);
+}).catch(err => {
+    try {
+            console.log('ERROR:', err.response.data);
 
-        return $('<tr>').append(tableData);
-    });
+    console.log(err.message);
 
-    container.append(tableRows);
-};
+    const newError = new Error('This is the error message');
+
+    console.log(newError.message);
+
+    throw(newError);
+    
+    alert(err.response.data.error);
+    } catch(error){
+        alert(error.message);
+    }
+
+});
